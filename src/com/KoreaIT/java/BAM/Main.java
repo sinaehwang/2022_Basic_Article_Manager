2dab61343f8db66518035380bcc45dcd7d92184f
sinaehwang <godvv29756318@gmail.com>
2022-07-13 18:31:19 +0900

    게시물구현하기1

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -5,9 +5,42 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("===프로그램시작====");
 		Scanner sc = new Scanner(System.in);
-		String command = sc.nextLine();
-		System.out.printf("입력된명령어:%s\n",command);
+		
+		while(true) {
+			System.out.println("명령어)");
+			String command = sc.next();
+			if(command.equals("exit")) {
+				break;
+			}
+
+			else {
+				System.out.println("존재하지않습니다.");				
+			}
+			}
+		
+			if(command.equals("article write")) {
+				String text = sc.next();
+				String body = sc.next();
+				System.out.printf("제목:%s",text);
+				System.out.printf("제목:%s",body);
+				System.out.println("1번글이 생성되었습니다.");
+			}
+			command = sc.next();
+			if(command.equals("article write")) {
+				String text = sc.next();
+				String body = sc.next();
+				System.out.printf("제목:%s",text);
+				System.out.printf("제목:%s",body);
+				System.out.println("2번글이 생성되었습니다.");
+			}
+			
+			else {
+				System.out.println("잘못입력됬습니다");
+			}
+			command = sc.next();
+		}
 		System.out.println("===프로그램종료=====");
 		
 	}
 }
+
