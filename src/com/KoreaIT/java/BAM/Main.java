16b79167b40351a4e3a4b29fbbc02b46ff3b2d6d
sinaehwang <godvv29756318@gmail.com>
2022-07-13 16:46:38 +0900

    github 업로드연습

---
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -0,0 +1,13 @@
+package com.KoreaIT.java.BAM;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("===프로그램시작====");
+		Scanner sc = new Scanner(System.in);
+		String command = sc.nextLine();
+		System.out.println(command);
+		System.out.println("===프로그램종료=====");
+
+	}
+}
