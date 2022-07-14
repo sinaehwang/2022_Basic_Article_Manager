b4a804e97c6e257735467c0ea1749881a56308b9
sinaehwang <godvv29756318@gmail.com>
2022-07-14 18:37:02 +0900

    게시판상세페이지구현,날짜(x)

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -5,86 +5,89 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("===ÇÁ·Î±×·¥½ÃÀÛ====");
-=======
-		System.out.println("===í”„ë¡œê·¸ë¨ì‹œì‘====");
->>>>>>> c6f39e1cb4652a4a82b459d81f88b039fb9a8692
+		System.out.println("==í”„ë¡œê·¸ë¨ ì‹œì‘==");
 
 		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
 
-		int num = 0;
-<<<<<<< HEAD
-		List<Article>articles = new ArrayList<>();
-		
-		while (true) {//"¸í·É¾î ½ÇÇà¹®À» ¹«ÇÑ¹İº¹½ÃÅ²´Ù.
-			System.out.print("¸í·É¾î)");
-			String command = sc.nextLine().trim();
-			if(command.length()==0) {
-				continue; //¸í·É¾î ÀÔ·ÂÀ» ¾ÈÇÑ °æ¿ìÀÇ ¼ö¸¦ ¸¸µé¾îÁà¾ß "Á¸ÀçÇÏÁö¾Ê´Â~"ÀÌ ½ÇÇàµÇÁö¾Ê´Â´Ù.**continue·Î ¸ÇÃ³À½À¸·Î µ¹¾Æ°¡°ÔÇÔ
-=======
-		while (true) {//"ëª…ë ¹ì–´ ì‹¤í–‰ë¬¸ì„ ë¬´í•œë°˜ë³µì‹œí‚¨ë‹¤.
-			System.out.print("ëª…ë ¹ì–´)");
-			String command = sc.nextLine().trim();
-			if(command.length()==0) {
-				continue; //ëª…ë ¹ì–´ ì…ë ¥ì„ ì•ˆí•œ ê²½ìš°ì˜ ìˆ˜ë¥¼ ë§Œë“¤ì–´ì¤˜ì•¼ "ì¡´ì¬í•˜ì§€ì•ŠëŠ”~"ì´ ì‹¤í–‰ë˜ì§€ì•ŠëŠ”ë‹¤.**continueë¡œ ë§¨ì²˜ìŒìœ¼ë¡œ ëŒì•„ê°€ê²Œí•¨
->>>>>>> c6f39e1cb4652a4a82b459d81f88b039fb9a8692
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+
+			System.out.printf("ëª…ë ¹ì–´ ) ");
+			String cmd = sc.nextLine().trim();
+			if (cmd.length() == 0) {
+				System.out.println("ëª…ë ¹ì–´ë¥¼ ì…ë ¥í•´ì£¼ì„¸ìš”");
+				continue;
 			}
 
-			if (command.equals("exit")) {
+			if (cmd.equals("exit")) {
 				break;
-			} 
-<<<<<<< HEAD
-			if (command.equals("article list")) {
-				if(articles.size()==0) {
-					System.out.println("°Ô½Ã±ÛÀÌ ¾ø½À´Ï´Ù.");
+			}
+
+			if (cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("ì œëª© : ");
+				String title = sc.nextLine();
+				System.out.printf("ë‚´ìš© : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%dë²ˆ ê¸€ì´ ìƒì„±ë˜ì—ˆìŠµë‹ˆë‹¤\n", id);
+
+			} else if (cmd.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("ê²Œì‹œë¬¼ì´ ì—†ìŠµë‹ˆë‹¤");
 					continue;
 				}
-				else {
-					for(int i=articles.size()-1; i>=0; i--) {
-						Article article = articles.get(i);//articles.get(i)ÀÎµ¦½º°ªÀ» article »õº¯¼öÀúÀå½ÃÄÑ¼­ »ç¿ëÇÑ´Ù.
-						System.out.println("¹øÈ£/Á¦¸ñ\n"+article.id+article.title);
+				System.out.println("ë²ˆí˜¸    |   ì œëª©");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+
+			} else if (cmd.startsWith("article detail ")) {
+
+				String[] cmdBits = cmd.split(" ");
+
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
 					}
 				}
-			} 
-			
-			else if (command.equals("article write")) {
-				int id = num+1;
-				num++;//¹İº¹¹® ¹Û¿¡ ¼±¾ğÇß±â¶§¹®¿¡ num°ªÀº °è¼Ó Áõ°¡°ªÀÌµÈ´Ù.
-				String title = sc.nextLine();
-				String body = sc.nextLine();
-				System.out.printf("Á¦¸ñ:%s\n", title);
-				System.out.printf("º»¹®:%s\n", body);
-				System.out.printf("%d¹ø±ÛÀÌ»ı¼º‰ç½À´Ï´Ù.\n", id);
-				Article article = new Article(id, title, body);
-				articles.add(article);//articles ¹è¿­¿¡ ÀúÀåÇÑ´Ù.
-			}
 
-			else {
-				System.out.println("Á¸ÀçÇÏÁö ¾Ê´Â ¸í·É¾îÀÔ´Ï´Ù.");
-=======
-			else if (command.equals("article list")) {
-				System.out.println("ê²Œì‹œê¸€ì´ ì—†ìŠµë‹ˆë‹¤.");
-			} 
-			else if (command.equals("article write")) {
-				int id = num+1;
-				num++;//ë°˜ë³µë¬¸ ë°–ì— ì„ ì–¸í–ˆê¸°ë•Œë¬¸ì— numê°’ì€ ê³„ì† ì¦ê°€ê°’ì´ëœë‹¤.
-				String article = sc.nextLine();
-				String body = sc.nextLine();
-				System.out.printf("ì œëª©:%s\n", article);
-				System.out.printf("ë³¸ë¬¸:%s\n", body);
-				System.out.printf("%dë²ˆê¸€ì´ìƒì„±ë¬ìŠµë‹ˆë‹¤.\n", id);
-			}
+				if (foundArticle == null) {
+					System.out.printf("%dë²ˆ ê²Œì‹œë¬¼ì€ ì—†ì–´\n", id);
+					continue;
+				} else {
+					System.out.printf("ë²ˆí˜¸ : %d\n", foundArticle.id);
+					System.out.printf("ë‚ ì§œ : 2022-12-12 12:12:12\n");
+					System.out.printf("ì œëª© : %s\n", foundArticle.title);
+					System.out.printf("ë‚´ìš© : %s\n", foundArticle.body);
+				}
 
-			else {
-				System.out.println("ì¡´ì¬í•˜ì§€ ì•ŠëŠ” ëª…ë ¹ì–´ì…ë‹ˆë‹¤.");
->>>>>>> c6f39e1cb4652a4a82b459d81f88b039fb9a8692
+			} else {
+				System.out.println("ì¡´ì¬í•˜ì§€ ì•ŠëŠ” ëª…ë ¹ì–´ì…ë‹ˆë‹¤");
 			}
 		}
-		System.out.println("===í”„ë¡œê·¸ë¨ì¢…ë£Œ=====");
-		
+
+		System.out.println("==í”„ë¡œê·¸ë¨ ë==");
+		sc.close();
 	}
+
 }
+
 class Article {
 	int id;
 	String title;
