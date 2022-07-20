3da34ebab9c958a299e990a593c4e81f2efee1f8
sinaehwang <godvv29756318@gmail.com>
2022-07-20 09:04:03 +0900

    회원가입 기능 구현중 아이디중복체크와 비밀번호 확인구현필요

---
+++ src/com/KoreaIT/java/BAM/dto/Member.java
@@ -0,0 +1,21 @@
+package com.KoreaIT.java.BAM.dto;
+
+public class Member extends Dto {
+	
+	public String loginId;
+	public String loginPw;
+	public String name;
+
+	public Member(int id, String regDate, String loginId, String loginPw,String loginPwConfirm) {
+		this.id = id;
+		this.regDate = regDate;
+		this.loginId = loginId;
+		this.loginPw = loginPw;
+		this.name = name;
+		
+	}
+	
+
+
+	
+}
