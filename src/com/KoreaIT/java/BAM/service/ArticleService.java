ac51dbb0b72f0a47f838026e36281c098b4308bd
sinaehwang <godvv29756318@gmail.com>
2022-07-28 18:36:09 +0900

    html 추출기능 도입

--- src/com/KoreaIT/java/BAM/service/ArticleService.java
+++ src/com/KoreaIT/java/BAM/service/ArticleService.java
@@ -23,9 +23,7 @@ public class ArticleService {
 
 	public int setNewId() {
 
-		int id = articleDao.setNewId();
-
-		return id;
+		return articleDao.setNewId();
 	}
 
 	public void add(Article article) {
@@ -38,5 +36,9 @@ public class ArticleService {
 
 	public void remove(Article foundArticle) {
 		articleDao.remove(foundArticle);
+	}
+
+	public List<Article> getForPrintArticles() {
+		return articleDao.getArticles(null);
 	}
 }
